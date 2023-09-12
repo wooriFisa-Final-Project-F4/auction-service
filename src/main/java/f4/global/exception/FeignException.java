cd7bee4420990460a02a1b47a31ce8d9d1144a58
KHJ <rlagurwnsid2@gmail.com>
2023-09-12 17:28:58 +0900

    [log] feign error log

--- src/main/java/f4/global/exception/FeignException.java
+++ src/main/java/f4/global/exception/FeignException.java
@@ -2,7 +2,9 @@ package f4.global.exception;
 
 import java.net.ConnectException;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 public class FeignException extends RuntimeException {
 
@@ -11,5 +13,6 @@ public class FeignException extends RuntimeException {
   public FeignException(Object object) {
     super("Feign 통신 중 에러가 발생했습니다.");
     this.object = object;
+    log.info("FeignException Object : {}",object.toString());
   }
 }
